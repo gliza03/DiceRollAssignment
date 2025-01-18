a2be5335cfa32e84e2982d00e1e0f13983c3c453
gabe <gabrielliza012@gmail.com>
2025-01-17 23:02:23 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Mission2Assign;
+
+internal class Class1
+{
+    private Random rnd = new Random();
+
+    public int[] SimulateRolls(int rolls)
+    {
+        int[] results = new int[13];
+        while (rolls > 0)
+        {
+            int dice1 = rnd.Next(1, 7);
+            int dice2 = rnd.Next(1, 7);
+
+            int sum = dice1 + dice2;
+            results[sum]++;
+            rolls--;
+        }
+        return results;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Mission2Assign;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+        string stringNum = Console.ReadLine();
+        int number = int.Parse(stringNum);
+
+        Class1 simulator = new Class1();
+
+        int[] results = simulator.SimulateRolls(number);
+
+        // Print results
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {number}.\n");
+
+        // Print histogram
+        for (int i = 2; i <= 12; i++)
+        {
+            // Calculate percentage and round to nearest integer
+            int percentage = (int)Math.Round((double)results[i] / number * 100);
+            string stars = "";
+
+            // Add stars
+            for (int j = 0; j < percentage; j++)
+            {
+                stars += "*";
+            }
+
+            Console.WriteLine($"{i}: {stars}");
+        }
+
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+
+
+    }
+}
